fcb12a2e2f09fa730f04319253ce013c362fc95d
LucasLorencetti1904 <lucaslorencetti222@outlook.com>
2025-03-13 07:53:14 -0300

    feat: adicionar validações de erro próprias para formulário vazio

--- projeto-frontend-rotas-http/assets/script.js
+++ projeto-frontend-rotas-http/assets/script.js
@@ -1,34 +1,41 @@
 const result = document.getElementById("result")
 
+const formPerfil = document.getElementById("formPerfil")
+const formTubo = document.getElementById("formTubo")
+
+const inputsTubo = formTubo.querySelectorAll('input, select')
+const inputsPerfil = formPerfil.querySelectorAll('input, select')
+
+const inputsGerais = document.querySelectorAll("input, select")
+inputsGerais.forEach((input) => {
+    input.addEventListener("input", () => {
+        if (input.value.trim()) {
+            ocultarErro(input)
+            return
+        }
+    })
+})
+
 const tipo = document.getElementById("tipo")
 tipo.addEventListener("change", () => {
-    
-    const formPerfil = document.getElementById("formPerfil")
-    const formTubo = document.getElementById("formTubo")
-    
-    let inputsTubo = formTubo.querySelectorAll('input, select')
-    let inputsPerfil = formPerfil.querySelectorAll('input, select')
     
     if (tipo.value == "tubo") {
         formPerfil.classList.add("oculto")
         formTubo.classList.remove("oculto")
-        inputsTubo.forEach((input) => {
-            input.setAttribute('required', true)
-        })
-        inputsPerfil.forEach((input) => {
-            input.removeAttribute('required')
-        })
         
+        inputsPerfil.forEach((input) => {
+            input.value = ""
+            ocultarErro(input)
+        })
         return
     }
     
     formTubo.classList.add("oculto")
     formPerfil.classList.remove("oculto")
-    inputsPerfil.forEach((input) => {
-        input.setAttribute('required', true)
-    })
+    
     inputsTubo.forEach((input) => {
-        input.removeAttribute('required')
+        input.value = ""
+        ocultarErro(input)
     })
     
 })
@@ -38,16 +45,52 @@ formulario.addEventListener("submit", async (e) => {
     
     e.preventDefault()
     
+    if (!tipo.value.trim()) {
+        exibirErro(tipo)
+        return
+    }
+    
+    const arrayInputs = Array.from((tipo.value == "tubo"
+        ? inputsTubo
+        : inputsPerfil
+    ))
+    
+    arrayInputs.forEach((input) => {
+        !input.value.trim() 
+            ? exibirErro(input)
+            : ocultarErro(input)
+    })
+    const erro = arrayInputs.some((input) => !input.value.trim())
+    
+    if (erro) return
+    
     const dados = {}
     const inputs = new FormData(formulario)
     
     inputs.forEach((valor, propriedade) => {
-        dados[propriedade] = valor
+        if (valor) {
+            dados[propriedade] = valor
+        }
     })
     
     const endpoint = "http://localhost:3000/salvar"
+
+    try {
+        const dados = await salvarFerramenta(endpoint)
+    }
+    catch(error) {
+        console.error(error)
+    }
     
-    return await salvarFerramenta(endpoint)
+    Array.from(inputsGerais)
+        .filter(e => e.id != "tipo")
+        .forEach((input) => input.value = "")
+    
+    alert(
+        dados.tipo == "tubo"
+            ? `${dados.polegadas}"${dados.matriz}`
+            : `${dados.empresa}/${dados.cliente}${dados.categoria.toUpperCase()}`
+    )
     
 })
 
@@ -66,9 +109,8 @@ const salvarFerramenta = async (url) => {
         }
         
         const data = await response.json()
-        result.innerHTML = data
         
-        return console.log(data)
+        return data
         
     }
     
@@ -76,4 +118,14 @@ const salvarFerramenta = async (url) => {
         return console.error(`Erro de conexão: \n\n${error.message}`)
     }
     
+}
+
+function exibirErro(elemento) {
+    elemento.classList.add("is-invalid")
+    elemento.nextElementSibling.display = "block"
+}
+
+function ocultarErro(elemento) {
+    elemento.classList.remove("is-invalid")
+    elemento.nextElementSibling.display = "none"
 }
